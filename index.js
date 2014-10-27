51f1a62740922f7920653f6a5c89ec30a3e4e035
chris <wfsr@foxmail.com>
2014-10-27 18:10:13 +0800

    对外暴露 cli 目录下模块的 run 方法

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * @file command line interface
+ * @author chris<wfsr@foxmail.com>
+ */
+
+var fs = require('fs');
+var path = require('path');
+
+var reg = /(.+)\.js$/i;
+var dir = path.join(__dirname, './cli');
+fs.readdirSync(dir).forEach(function (file) {
+    var match = file.match(reg);
+    if (match) {
+        var key = match[1].replace(/\-[a-z]/g, function (a) {
+            return a[1].toUpperCase();
+        });
+
+        exports[key] = require(path.join(dir, file)).run;
+    }
+});
