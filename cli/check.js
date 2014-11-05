15120b5d1b32e01ada7d61f2be2490dd293d3d9c
chris <wfsr@foxmail.com>
2014-11-05 15:05:13 +0800

    handle stdin & file stream

--- cli/check.js
+++ cli/check.js
@@ -9,7 +9,60 @@ var util       = require('../lib/util');
 var jschecker  = require('../lib/js/checker');
 var csschecker = require('../lib/css/checker');
 
+/**
+ * 不同的输入流处理
+ *
+ * @namespace
+ */
+var streams = {
 
+    /**
+     * 处理文件系统中的代码
+     *
+     * @param {Object} options minimist 处理后的 cli 参数
+     */
+    files: function (options) {
+        var types = options.t || options.type || 'js,css,html';
+
+        var extensions = types;
+        types = types.split(/\s*,\s*/);
+        if (types.length > 1) {
+            extensions = '{' + types.join(',') + '}';
+        }
+
+        var dirs = options._;
+        if (!dirs.length) {
+            dirs = ['./'];
+        }
+
+        var patterns = util.buildPattern(dirs, extensions);
+
+        return fs.src(patterns, {cwdbase: true})
+            .pipe(jschecker(options))
+            .pipe(csschecker(options));
+    },
+
+    /**
+     * 处理从 stdin 输入的代码
+     *
+     * @param {Object} options minimist 处理后的 cli 参数
+     */
+    stdin: function (options) {
+        var through = require('through2');
+        var File = require('vinyl');
+
+        var type = (options.t || options.type || 'js').split(',')[0];
+        var handler = type === 'js' ? jschecker(options) : (type === 'css' ? csschecker(options) : through());
+
+        return process.stdin
+            .pipe(
+                through.obj(function (chunk, enc, cb) {
+                    cb(null, new File({contents: chunk, path: 'current-file.' + type}));
+                }
+            ))
+            .pipe(handler);
+    }
+};
 
 /**
  * check 处理入口
@@ -20,26 +73,9 @@ exports.run = function (options) {
     console.time('fecs');
 
     var log = require('../lib/log')(options.color);
-    var types = options.t || options.type || 'js,css,html';
-
-    var extensions = types;
-    types = types.split(/\s*,\s*/);
-    if (types.length > 1) {
-        extensions = '{' + types.join(',') + '}';
-    }
-
-    var dirs = options._;
-    if (!dirs.length) {
-        dirs = ['./'];
-    }
-
-    var patterns = util.buildPattern(dirs, extensions);
     var reporter = require('../lib/reporter').get(log, options);
 
-    fs.src(patterns, {cwdbase: true})
-        .pipe(jschecker(options))
-        .pipe(csschecker(options))
-        // .pipe(htmlchecker(options))
+    streams[options.stream ? 'stdin' : 'files'](options)
         .pipe(reporter)
         .once('end', function (success) {
             console.timeEnd('fecs');
