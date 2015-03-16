892536add1bcfc827d8a5643c9bdb0ed8904bdfe
chris <wfsr@foxmail.com>
2015-03-16 14:55:55 +0800

    restore variable name to `error`

--- test/lib/util.spec.js
+++ test/lib/util.spec.js
@@ -38,17 +38,17 @@ describe('util', function () {
                 throw new Error('foo', 'bar');
             }
             catch (e) {
-                var errorObj = util.parseError({foo: 'bar'}, e);
+                var error = util.parseError({foo: 'bar'}, e);
 
-                expect(errorObj).not.toBeNull();
-                expect(errorObj.foo).toBe('bar');
+                expect(error).not.toBeNull();
+                expect(error.foo).toBe('bar');
 
                 // 行列信息必须对应上面 throw new 的位置
                 //                         ^
                 // 有变化时必须更正以下两个期望值
-                expect(errorObj.line).toBe(38);
-                expect(errorObj.column).toBe(23);
-                expect(errorObj.message).toMatch(/foo\([^\)]+\)/);
+                expect(error.line).toBe(38);
+                expect(error.column).toBe(23);
+                expect(error.message).toMatch(/foo\([^\)]+\)/);
             }
         });
 
