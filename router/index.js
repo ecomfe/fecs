c8b215bc6617cf54f7a53a51951bfa3e854eb22e
chris <wfsr@foxmail.com>
2015-10-23 14:19:37 +0800

    fix code style and route fecs

--- router/index.js
+++ router/index.js
@@ -1,8 +1,26 @@
-var controller = require('../controller/index');
+/**
+ * @file 路由配置
+ * @author chris<wfsr@foxmail.com>
+ */
 
-module.exports = function(app){
-    app.get('/', controller.home);
-    app.get('/api', controller.api);
-    app.get('/demo', controller.demo);
-    app.get('/plugins', controller.plugins);
+var route = require('koa-route');
+
+var page = require('../controller/index');
+var fecs = require('../controller/fecs');
+
+module.exports = function (app) {
+
+    Object.keys(route).forEach(function (method) {
+        app[method] = function () {
+            app.use(route[method].apply(route, arguments));
+        };
+    });
+
+    app.get('/', page.home);
+    app.get('/api', page.api);
+    app.get('/demo', page.demo);
+    app.get('/plugins', page.plugins);
+
+    app.post('/check/:type/:reporter/', fecs.check);
+    app.post('/format/:type/', fecs.format);
 };
