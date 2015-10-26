87addae08bbef4e8ee845f260975de87aaf00c45
cgzero <cgzero@foxmail.com>
2015-10-26 18:09:22 +0800

    demo 页面增加 format 功能和全屏功能

--- public/js/demo.js
+++ public/js/demo.js
@@ -6,134 +6,119 @@
 (function () {
 
     /**
-     * 验证js代码的地址
+     * 代码检测、格式化地址
      *
      * @const
-     * @type {string}
+     * @type {Object}
      */
-    var URL_CHECK_JS = '/check/js/baidu';
-
-    /**
-     * 验证css代码的地址
-     *
-     * @const
-     * @type {string}
-     */
-    var URL_CHECK_CSS= '/check/css/baidu';
-
-    /**
-     * 验证html代码的地址
-     *
-     * @const
-     * @type {string}
-     */
-    var URL_CHECK_HTML = '/check/html/baidu';
-
-    /**
-     * 验证less代码的地址
-     *
-     * @const
-     * @type {string}
-     */
-    var URL_CHECK_LESS = '/check/less/baidu';
-
-    var SERVERITY = {
-        1: '<span class="warn">&nbsp;WARN</span>',
-        2: '<span class="err">ERROR</span>'
+    var URL = {
+        // 检测js地址
+        CHECK_JS: '/check/js/baidu',
+        // 检测css地址
+        CHECK_CSS: '/check/css/baidu',
+        // 检测html地址
+        CHECK_HTML: '/check/html/baidu',
+        // 检测less地址
+        CHECK_LESS: '/check/less/baidu',
+        // 格式化js地址
+        FORMAT_JS: '/format/js',
+        // 格式化css地址
+        FORMAT_CSS: '/format/css',
+        // 格式化html地址
+        FORMAT_HTML: '/format/html',
+        // 格式化less地址
+        FORMAT_LESS: '/format/less'
     };
 
     /**
-     * fecs检查通过的信息
+     * 模板常量
      *
      * @const
-     * @type {string}
+     * @type {Object}
      */
-    var TPL_CONSOLE_OK = '<div>fecs <span class="info">INFO</span> Congratulations! Everything is OK!</div>';
+    var TPL = {
+        // fecs检查通过的信息
+        CONSOLE_OK: '<div>fecs <span class="info">INFO</span> Congratulations! Everything is OK!</div>',
+        // fecs检查出错的信息
+        CONSOLE_WRAN: '<div>fecs ${serverity} → line ${line}, col ${col}: ${msg}</div>',
+        // 代码检查时，每种类型错误所对应的模板
+        SERVERITY: {
+            1: '<span class="warn">&nbsp;WARN</span>',
+            2: '<span class="err">ERROR</span>'
+        }
+    };
 
     /**
-     * fecs检查出错的信息
+     * 初始化的代码片段
      *
      * @const
-     * @type {string}
+     * @type {Object}
      */
-    var TPL_CONSOLE_WRAN = '<div>fecs ${serverity} → line ${line}, col ${col}: ${msg}</div>';
+    var CODE = {
+        // js 初始化代码
+        CODE_JS: [
+            '/**',
+            ' * @file FECS test file',
+            ' * @author cgzero(cgzero@cgzero.com)',
+            ' */',
+            '',
+            '/**',
+            ' * say Hello',
+            ' */',
+            'function sayHello() {',
+            '    alert(\'Hello FECS\');',
+            '}',
+            '',
+            'sayHello();',
+            ''
+        ],
+        // css 初始化代码
+        CODE_CSS: [
+            'body {',
+            '    margin: 0;',
+            '    padding: 0;',
+            '}',
+            ''
+        ],
+        // html 初始化代码
+        CODE_HTML: [
+            '<!DOCTYPE html>',
+            '<html lang="zh-CN">',
+            '<head>',
+            '    <meta charset="UTF-8">',
+            '    <title>Hello FECS</title>',
+            '    <meta http-equiv="X-UA-Compatible" content="IE=Edge">',
+            '    <meta name="viewport" content="width=device-width, initial-scale=1">',
+            '</head>',
+            '<body>',
+            '    Hello FECS',
+            '</body>',
+            '</html>'
+        ],
+        // less 初始化代码
+        CODE_LESS: [
+            '@zero: 0;',
+            '',
+            'body {',
+            '    margin: $zero;',
+            '    padding: $zero;',
+            '}',
+            ''
+        ]
+    };
 
     /**
-     * js 初始化代码
+     * 各种文字提示信息
      *
      * @const
-     * @type {string}
+     * @type {Object}
      */
-    var CODE_JS = [
-        '/**',
-        ' * @file FECS test file',
-        ' * @author cgzero(cgzero@cgzero.com)',
-        ' */',
-        '',
-        '/**',
-        ' * say Hello',
-        ' */',
-        'function sayHello() {',
-        '    alert(\'Hello FECS\');',
-        '}',
-        '',
-        'sayHello();',
-        ''
-    ];
-
-    /**
-     * css 初始化代码
-     *
-     * @const
-     * @type {string}
-     */
-    var CODE_CSS = [
-        'body {',
-        '    margin: 0;',
-        '    padding: 0;',
-        '}'
-    ];
-
-    /**
-     * html 初始化代码
-     *
-     * @const
-     * @type {string}
-     */
-    var CODE_HTML = [
-        '<!DOCTYPE html>',
-        '<html lang="zh-CN">',
-        '<head>',
-        '    <meta charset="UTF-8"/>',
-        '    <title>Hello FECS</title>',
-        '    <meta name="viewport" content="width=device-width, initial-scale=1">',
-        '</head>',
-        '<body>Hello FECS</body>',
-        '</html>'
-    ];
-
-    /**
-     * less 初始化代码
-     *
-     * @const
-     * @type {string}
-     */
-    var CODE_LESS = [
-        '@zero: 0;',
-        '',
-        'body {',
-        '    margin: $zero;',
-        '    padding: $zero;',
-        '}'
-    ];
-
-    /**
-     * 检查错误时的提示信息
-     *
-     * @const
-     * @type {string}
-     */
-    var MSG_CHECK_ERR = '服务器貌似出错了，无法检查代码:(';
+    var MSG = {
+        // 检查错误时的提示信息
+        CHECK_ERR: '服务器貌似出错了，无法检查代码:(',
+        // 格式化错误时的提示信息
+        FORMAT_ERR: '服务器貌似出错了，无法格式化代码:('
+    };
 
     /**
      * 动态验证的延迟时间
@@ -144,11 +129,25 @@
     var CHECK_DELAY_TIME = 500;
 
     /**
+     * ace编辑器
+     *
+     * @type {Object}
+     */
+    var ace = window.ace;
+
+    /**
      * 为checkCode增加防抖功能
      *
      * @type {Function}
      */
     var checkCodeDebounce = debounce(CHECK_DELAY_TIME, checkCode);
+
+    /**
+     * 存放编辑器实例的列表
+     *
+     * @type {Object}
+     */
+    var editorMap = {};
 
     /**
      * 字符串格式化
@@ -208,20 +207,20 @@
 
         switch (type) {
             case 'javascript':
-                codeTxt = CODE_JS.join('\n');
-                url = URL_CHECK_JS;
+                codeTxt = CODE.CODE_JS.join('\n');
+                url = URL.CHECK_JS;
                 break;
             case 'css':
-                codeTxt = CODE_CSS.join('\n');
-                url = URL_CHECK_CSS;
+                codeTxt = CODE.CODE_CSS.join('\n');
+                url = URL.CHECK_CSS;
                 break;
             case 'html':
-                codeTxt = CODE_HTML.join('\n');
-                url = URL_CHECK_HTML;
+                codeTxt = CODE.CODE_HTML.join('\n');
+                url = URL.CHECK_HTML;
                 break;
             case 'less':
-                codeTxt = CODE_LESS.join('\n');
-                url = URL_CHECK_LESS;
+                codeTxt = CODE.CODE_LESS.join('\n');
+                url = URL.CHECK_LESS;
                 break;
         }
 
@@ -231,18 +230,22 @@
         editor.getSession().setUseWorker(false);
         editor.getSession().setMode('ace/mode/' + type);
         editor.getSession().setValue(codeTxt);
-
-        editor.setOption("wrap",false);
+        editor.setOption('wrap', false);
         checkCode(editor.getSession().getValue(), type, url);
         editor.on('change', function (evt) {
             checkCodeDebounce(editor.getValue(), type, url);
         });
 
-        return editor;
+        editorMap[type] = editor;
     }
 
-    function initEditor() {
-        $('.nav-item').on('click', function() {
+    /**
+     * 初始化编辑器事件
+     *
+     * @inner
+     */
+    function initEditorEvent() {
+        $('.nav-item').on('click', function () {
             var self = $(this);
 
             $('.nav-item').removeClass('cur');
@@ -256,10 +259,75 @@
             $('#' + dataFor + '-console').show();
         });
 
+        $('.format-btn').on('click', function (event) {
+            var type = $('.nav-item.cur').attr('data-for');
+            var currEditer = editorMap[type];
+            var code = currEditer.getSession().getValue();
+            var url;
+
+            switch (type) {
+                case 'javascript':
+                    url = URL.FORMAT_JS;
+                    break;
+                case 'css':
+                    url = URL.FORMAT_CSS;
+                    break;
+                case 'html':
+                    url = URL.FORMAT_HTML;
+                    break;
+                case 'less':
+                    url = URL.FORMAT_LESS;
+                    break;
+            }
+
+            formatCode(code, type, url, function (data) {
+                currEditer.getSession().setValue(data.code);
+            });
+        });
+
+        var resizeEditor = function () {
+            if ($('.demo-editor-wrap').hasClass('full')) {
+                $('.demo-editor').height($(window).height() - 150);
+            }
+            else {
+                $('.demo-editor').height(500);
+            }
+
+            $.each(editorMap, function (key, editor) {
+                editor.resize();
+            });
+        };
+
+        $('.full-screen-btn').on('click', function () {
+            var self = $(this);
+            if (!self.hasClass('full')) {
+                $('.demo-editor-wrap').addClass('full');
+                self.addClass('full');
+                resizeEditor();
+            }
+            else {
+                $('.demo-editor-wrap').removeClass('full');
+                self.removeClass('full');
+                resizeEditor();
+            }
+        });
+
+        $(window).resize(function(event) {
+            resizeEditor();
+        });
+    }
+
+    /**
+     * 初始化编辑器
+     *
+     * @inner
+     */
+    function initEditor() {
         initAceEditor('javascript');
         initAceEditor('css');
         initAceEditor('html');
         initAceEditor('less');
+        initEditorEvent();
     }
 
     /**
@@ -272,13 +340,12 @@
     function showConsole(data, type) {
         var consoleTpl = '';
         if (!data.length) {
-            consoleTpl = TPL_CONSOLE_OK;
+            consoleTpl = TPL.CONSOLE_OK;
         }
         else {
-            $.each(data, function(i, item) {
-                console.log(item);
-                consoleTpl += stringFormat(TPL_CONSOLE_WRAN, {
-                    serverity: SERVERITY[item.severity],
+            $.each(data, function (i, item) {
+                consoleTpl += stringFormat(TPL.CONSOLE_WRAN, {
+                    serverity: TPL.SERVERITY[item.severity],
                     line: item.line,
                     col: item.colum,
                     msg: item.message
@@ -306,7 +373,6 @@
             type: 'POST',
             url: url,
             data: params,
-            cache: false,
             dataType: 'json'
         })
         .then(
@@ -314,7 +380,39 @@
                 showConsole(data, type);
             },
             function () {
-                alert(MSG_CHECK_ERR);
+                alert(MSG.CHECK_ERR);
+            }
+        );
+    }
+
+    /**
+     * 格式化代码
+     *
+     * @inner
+     * @param {string} code 代码片段
+     * @param {string} type 代码类型
+     * @param {string} url 请求路径
+     * @param {Function} callback 回调函数
+     */
+    function formatCode(code, type, url, callback) {
+        var params = {
+            code: code
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: params,
+            dataType: 'json'
+        })
+        .then(
+            function (data) {
+                if (callback) {
+                    callback(data);
+                }
+            },
+            function () {
+                alert(MSG.FORMAT_ERR);
             }
         );
     }
