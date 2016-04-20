6d41bd53ddf2b296bc93a307f8ec2e80f82ea6ff
chris <wfsr@foxmail.com>
2016-04-21 01:39:18 +0800

    add test case for `no-global-require` and fix bugs

---
+++ test/lib/js/rules/no-global-require.spec.js
@@ -0,0 +1,106 @@
+/**
+ * @file Check global require.
+ * @author chris<wfsr@foxmail.com>
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require('../../../../lib/js/rules/no-global-require');
+var RuleTester = require('eslint').RuleTester;
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester({env: {es6: true}});
+
+ruleTester.run('no-global-require', rule, {
+
+    valid: [
+        'var foo = require("foo");',
+        'require(["foo"], (foo) => {});',
+        'function define(require) {var foo = require("foo");}',
+        '() => {var foo = require("foo");}',
+        'define(function () {var require = String,foo = require("foo");})',
+        'define(function (require) {var foo = require("foo");})',
+        'define(function () {require(["foo"], (foo) => {});})',
+        'define(function () {window.require(["foo"]);})',
+        'var require = eval;require("foo=true");',
+        'define(function () {try {throw new Error("foo")} catch (require) {require("foo");}})',
+        'define(function (require) {try {throw new Error("foo")} catch (e) {require("foo");}})',
+        'define(function () {var esl = {require: eval, define: eval};with(esl) {require("foo");}});',
+        'define(function () {var esl;with(esl = {require: eval, define: eval}) {require("foo");}});',
+        'define(function () {with({require: eval, define: eval}) {require("foo");}});'
+    ],
+
+    invalid: [
+        {
+            code: 'define(function () {var foo = require("foo");})',
+            errors: [{
+                message: 'Expected a local require (Missing `require` in AMD factory).',
+                type: 'CallExpression'
+            }]
+        },
+        {
+            code: 'define(function (req) {var foo = require("foo");})',
+            errors: [{
+                message: 'Expected a local require (Missing `require` in AMD factory).',
+                type: 'CallExpression'
+            }]
+        },
+        {
+            code: 'define(function () {with(esl) {require("foo");}});',
+            errors: [
+                {
+                    message: 'Expected a local require (Missing `require` in AMD factory).',
+                    type: 'CallExpression'
+                }
+            ]
+        },
+        {
+            code: 'define(function () {var esl = {};with(esl) {require("foo");}});',
+            errors: [
+                {
+                    message: 'Expected a local require (Missing `require` in AMD factory).',
+                    type: 'CallExpression'
+                }
+            ]
+        },
+        {
+            code: 'define(function () {with(getObject()) {require("foo");}});',
+            errors: [
+                {
+                    message: 'Expected a local require (Missing `require` in AMD factory).',
+                    type: 'CallExpression'
+                }
+            ]
+        },
+        {
+            code: 'define(function () {try {throw new Error("foo");} catch (e) {\nrequire("foo");}})',
+            errors: [
+                {
+                    message: 'Expected a local require (Missing `require` in AMD factory).',
+                    line: 2,
+                    type: 'CallExpression'
+                }
+            ]
+        },
+        {
+            code: 'define(function () {try {\nrequire();} catch (require) {}\nrequire("foo");})',
+            errors: [
+                {
+                    message: 'Expected a local require (Missing `require` in AMD factory).',
+                    line: 2,
+                    type: 'CallExpression'
+                },
+                {
+                    message: 'Expected a local require (Missing `require` in AMD factory).',
+                    line: 3,
+                    type: 'CallExpression'
+                }
+            ]
+        }
+    ]
+});
