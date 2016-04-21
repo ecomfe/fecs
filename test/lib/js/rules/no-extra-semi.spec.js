b4ce9cb03241f6a41d130d776c1d23ed784e3533
chris <wfsr@foxmail.com>
2016-04-21 15:24:49 +0800

    add test cases for `fecs-no-extra-semi`

---
+++ test/lib/js/rules/no-extra-semi.spec.js
@@ -0,0 +1,133 @@
+/**
+ * @file Check `Super` callings.
+ * @author chris<wfsr@foxmail.com>
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require('../../../../lib/js/rules/no-extra-semi');
+var RuleTester = require('eslint').RuleTester;
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester({parser: 'babel-eslint'});
+
+ruleTester.run('no-extra-semi', rule, {
+    valid: [
+        'var x = 5;',
+        'function foo(){}',
+        'for(;;);',
+        'while(0);',
+        'do;while(0);',
+        'for(a in b);',
+        'for(a of b);',
+
+        // Class body.
+        'class A { }',
+        'var A = class { };',
+        'class A { a() { this; } }',
+        'var A = class { a() { this; } };',
+        'class A {a: true;}',
+        'class A { } a;',
+
+        // modules
+        'export const x = 42;',
+        'export default 42;',
+
+        // IIFE
+        ';(function () {})();',
+        ';(function () {}());',
+
+        // Decorator
+        'class A {@deco;["a"]() {}}'
+    ],
+    invalid: [
+        {
+            code: ';var x = 5;',
+            errors: [{message: 'Unnecessary semicolon.', type: 'EmptyStatement'}]
+        },
+        {
+            code: 'var x = 5;;',
+            errors: [{message: 'Unnecessary semicolon.', type: 'Program'}]
+        },
+        {
+            code: 'function foo(){};',
+            errors: [{message: 'Unnecessary semicolon.', type: 'FunctionDeclaration'}]
+        },
+        {
+            code: 'export default function foo(){};',
+            errors: [
+                {message: 'Unnecessary semicolon.', type: 'ExportDefaultDeclaration'}
+            ]
+        },
+        {
+            code: 'export function foo(){};',
+            errors: [
+                {message: 'Unnecessary semicolon.', type: 'ExportNamedDeclaration'}
+            ]
+        },
+        {
+            code: 'for(;;);;',
+            errors: [{message: 'Unnecessary semicolon.', type: 'ForStatement'}]
+        },
+        {
+            code: 'while(0);;',
+            errors: [{message: 'Unnecessary semicolon.', type: 'WhileStatement'}]
+        },
+        {
+            code: 'do;while(0);;',
+            errors: [{message: 'Unnecessary semicolon.', type: 'Program'}]
+        },
+        {
+            code: 'for(a in b);;',
+            errors: [{message: 'Unnecessary semicolon.', type: 'ForInStatement'}]
+        },
+        {
+            code: 'for(a of b);;',
+
+            errors: [{message: 'Unnecessary semicolon.', type: 'ForOfStatement'}]
+        },
+
+        // Class body.
+        {
+            code: 'class A { ; }',
+            errors: [{message: 'Unnecessary semicolon.', type: 'ClassBody', column: 11}]
+        },
+        {
+            code: 'class A { /*a*/; }',
+            errors: [{message: 'Unnecessary semicolon.', type: 'ClassBody', column: 16}]
+        },
+        {
+            code: 'class A { ; a() {} }',
+            errors: [{message: 'Unnecessary semicolon.', type: 'ClassBody', column: 11}]
+        },
+        {
+            code: 'class A { a() {}; }',
+            errors: [{message: 'Unnecessary semicolon.', type: 'MethodDefinition', column: 17}]
+        },
+        {
+            code: 'class A { a() {}; b() {} }',
+            errors: [{message: 'Unnecessary semicolon.', type: 'MethodDefinition', column: 17}]
+        },
+        {
+            code: 'class A {; a() {}; b() {}; }',
+            errors: [
+                {message: 'Unnecessary semicolon.', type: 'ClassBody', column: 10},
+                {message: 'Unnecessary semicolon.', type: 'MethodDefinition', column: 18},
+                {message: 'Unnecessary semicolon.', type: 'MethodDefinition', column: 26}
+            ]
+        },
+        // {
+        //     code: 'class A {\n@deco;\na() {}}',
+        //     errors: [{message: 'Unnecessary semicolon.', type: 'Decorator', column: 17}]
+        // },
+        {
+            code: 'class A { a() {}; get b() {} }',
+            errors: [{message: 'Unnecessary semicolon.', type: 'MethodDefinition', column: 17}]
+        }
+    ]
+});
