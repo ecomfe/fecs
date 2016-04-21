89f992e2c487eba4534865d27e3401548447aa06
chris <wfsr@foxmail.com>
2016-04-21 15:24:41 +0800

    add test cases for `fecs-imports-on-top`

---
+++ test/lib/js/rules/imports-on-top.spec.js
@@ -0,0 +1,53 @@
+/**
+ * @file Check the imports.
+ * @author chris<wfsr@foxmail.com>
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require('../../../../lib/js/rules/imports-on-top');
+var RuleTester = require('eslint').RuleTester;
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester({parser: 'babel-eslint'});
+
+ruleTester.run('imports-on-top', rule, {
+
+    valid: [
+        'import React from "react"',
+        '\nimport React from "react"\n',
+        '\n   \nimport React from "react"',
+        '\n   \nimport React from "react"\n ',
+        '\n   \nimport React from "react"\nimport {combineReducers} from "react-redux"',
+        '\nimport React from "react";function foo() {}'
+    ],
+
+    invalid: [
+        {
+            code: 'if (true) {}import React from "react";',
+            errors: [{
+                message: 'All `import` statements must be at the top of the module.',
+                type: 'ImportDeclaration'
+            }]
+        },
+        {
+            code: 'function foo() {}\nimport React from "react";',
+            errors: [{
+                message: 'All `import` statements must be at the top of the module.',
+                type: 'ImportDeclaration'
+            }]
+        },
+        {
+            code: ';;\nimport React from "react";function foo() {}',
+            errors: [{
+                message: 'All `import` statements must be at the top of the module.',
+                type: 'ImportDeclaration'
+            }]
+        }
+    ]
+});
