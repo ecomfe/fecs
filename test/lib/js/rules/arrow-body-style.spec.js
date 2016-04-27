46b9bc580774aac538d7cb4a283740981708242f
chris <wfsr@foxmail.com>
2016-04-27 15:04:36 +0800

    add rule `arrow-body-style`, fix #143, fix #144

---
+++ test/lib/js/rules/arrow-body-style.spec.js
@@ -0,0 +1,150 @@
+/**
+ * @fileoverview Tests for arrow-body-style
+ * @author Alberto Rodríguez
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require('../../../../lib/js/rules/arrow-body-style');
+var RuleTester = require('eslint').RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester({parser: 'babel-eslint'});
+
+ruleTester.run('arrow-body-style', rule, {
+    valid: [
+        'var foo = () => 0;',
+        'var addToB = (a) => { b =  b + a };',
+        'var foo = (retv, name) => {\nretv[name] = true;\nreturn retv;\n};',
+        'var foo = () => { bar(); };',
+        'var foo = () => { b = a };',
+        {
+            code: 'var foo = () => { return {}; };',
+            options: [
+                'as-needed',
+                {
+                    ObjectExpression: true
+                }
+            ]
+        },
+        {
+            code: 'var foo = () => { return 0; };',
+            options: ['always']
+        },
+        {
+            code: 'var foo = () => { return bar(); };',
+            options: ['always']
+        }
+    ],
+    invalid: [
+        {
+            code: 'var foo = () => {};',
+            errors: [
+                {
+                    line: 1,
+                    type: 'ArrowFunctionExpression',
+                    message: 'Expected block statement surrounding arrow body.'
+                }
+            ]
+        },
+        {
+            code: 'var foo = () => { /* do nothing */ };',
+            errors: [
+                {
+                    line: 1,
+                    type: 'ArrowFunctionExpression',
+                    message: 'Expected block statement surrounding arrow body.'
+                }
+            ]
+        },
+        {
+            code: 'var foo = () => {\n /* do nothing */ \n};',
+            errors: [
+                {
+                    line: 1,
+                    type: 'ArrowFunctionExpression',
+                    message: 'Expected block statement surrounding arrow body.'
+                }
+            ]
+        },
+        {
+            code: 'var foo = () => ({});',
+            options: ['always'],
+            errors: [
+                {
+                    line: 1,
+                    type: 'ArrowFunctionExpression',
+                    message: 'Expected block statement surrounding arrow body.'
+                }
+            ]
+        },
+        {
+            code: 'var foo = () => ({});',
+            options: [
+                'as-needed',
+                {
+                    ObjectExpression: true
+                }
+            ],
+            errors: [
+                {
+                    line: 1,
+                    type: 'ArrowFunctionExpression',
+                    message: 'Expected block statement surrounding arrow body.'
+                }
+            ]
+        },
+        {
+            code: 'var foo = () => { bar: 1 };',
+            errors: [
+                {
+                    line: 1,
+                    type: 'ArrowFunctionExpression',
+                    message: 'Expected block statement surrounding arrow body.'
+                }
+            ]
+        },
+        {
+            code: 'var foo = () => 0;',
+            options: ['always'],
+            errors: [
+                {
+                    line: 1,
+                    column: 17,
+                    type: 'ArrowFunctionExpression',
+                    message: 'Expected block statement surrounding arrow body.'
+                }
+            ]
+        },
+        {
+            code: 'var foo = () => { return 0; };',
+            options: ['as-needed'],
+            errors: [
+                {
+                    line: 1,
+                    column: 17,
+                    type: 'ArrowFunctionExpression',
+                    message: 'Unexpected block statement surrounding arrow body.'
+                }
+            ]
+        },
+        {
+            code: 'var foo = () => { return bar(); };',
+            options: ['as-needed'],
+            errors: [
+                {
+                    line: 1,
+                    column: 17,
+                    type: 'ArrowFunctionExpression',
+                    message: 'Unexpected block statement surrounding arrow body.'
+                }
+            ]
+        }
+    ]
+});
