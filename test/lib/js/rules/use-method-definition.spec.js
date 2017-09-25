89e9bd8d2862c18037584c67edc0995ecfcb3e20
chris <wfsr@foxmail.com>
2017-09-25 13:48:46 +0800

    允许单语句的函数使用箭头函数

--- test/lib/js/rules/use-method-definition.spec.js
+++ test/lib/js/rules/use-method-definition.spec.js
@@ -24,12 +24,15 @@ ruleTester.run('use-method-definition', rule, {
         'let foo = {["bar"]() {}}',
         'class Foo {bar() {}}',
         'class Foo extends Bar {bar() {}}',
-        'class Foo extends null {["bar"]() {}}'
+        'class Foo extends null {["bar"]() {}}',
+        'let foo = {bar: () => {}}',
+        'let foo = {bar: (n) => {return n * 10;}}',
+        'let foo = {baz: 1, bar: () => {this.baz++; return this.baz;}}'
     ],
 
     invalid: [
         {
-            code: 'let foo = {bar: () => {}}',
+            code: 'let foo = {bar: n => {var baz = n;return baz + n;}}',
             errors: [{
                 message: 'Expected MethodDefinition but saw ArrowFunctionExpression.',
                 type: 'ArrowFunctionExpression'
